1e37a0e4fd2fa6dd253718fab4505c3c9c2bcb2e
Fishmansky <paul96guitar@gmail.com>
2022-09-27 21:51:52 +0200

    Order Functions changed - 1st iteration

--- croupier/croupier.go
+++ croupier/croupier.go
@@ -3,21 +3,27 @@ package croupier
 import (
 	"fmt"
 	"math/rand"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
 )
 
-var HandsOrder = map[string]Hand{
-	"Royal Flush":    {Cards: []string{"10.*", "J.*", "Q.*", "K.*", "A.*"}},
+// best possible hands in descending order
+var BestHands = []string{"Royal Flush", "Straight Flush", "Four of kind", "Full House", "Flush", "Streigh", "Three of kind", "Two pairs", "Pair", "High Card"}
+
+var HandTypes = map[string]Hand{
+	// _ for discard
+	// * for wildcard
+	"Royal Flush":    {Cards: []string{"10.*", "11.*", "12.*", "13.*", "14.*"}},
 	"Straight Flush": {Cards: []string{"X.C", "X+1.C", "X+2.C", "X+3.C", "X+4.C"}}, // X for figure, C for Color
 	"Four of kind":   {Cards: []string{"X.*", "X.*", "X.*", "X.*", "*.*"}},
 	"Full House":     {Cards: []string{"X.*", "X.*", "X.*", "Y.*", "Y.*"}},
 	"Flush":          {Cards: []string{"*.C", "*.C", "*.C", "*.C", "*.C"}},
 	"Streigh":        {Cards: []string{"X.*", "X+1.*", "X+2.*", "X+3.*", "X+4.*"}},
 	"Three of kind":  {Cards: []string{"X.*", "X.*", "X.*", "Y.*", "Z.*"}},
-	"Two pairs":      {Cards: []string{"X.*", "X.*", "Y.*", "Y.*", "Z.*"}},
-	"Pair":           {Cards: []string{"X.*", "X.*", "Y.*", "Z.*", "V.*"}},
+	"Two pairs":      {Cards: []string{"X.*", "X.*", "Y.*", "Y.*", "_.*"}},
+	"Pair":           {Cards: []string{"X.*", "X.*", "_.*", "_.*", "_.*"}},
 	"High Card":      {Cards: []string{"X.*", "Y.*", "Z.*", "V.*", "Q.*"}},
 }
 
@@ -29,6 +35,17 @@ type Table struct {
 type Hand struct {
 	PlayerName string
 	Cards      []string
+}
+
+type Pair []int
+
+func contains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
 
 func Rank(card string) string {
@@ -64,18 +81,102 @@ func CardValues(cards []string) []int {
 	return convertedCards
 }
 
-func GroupCards(cards *[]string) []string {
-	return []string{}
+func CardRaks(cards []int) []string {
+	convertedCards := []string{}
+	for _, card := range cards {
+		switch card {
+		case 11:
+			convertedCards = append(convertedCards, "J")
+		case 12:
+			convertedCards = append(convertedCards, "Q")
+		case 13:
+			convertedCards = append(convertedCards, "K")
+		case 14:
+			convertedCards = append(convertedCards, "A")
+		default:
+			convertedCards = append(convertedCards, strconv.Itoa(card))
+		}
+	}
+	return convertedCards
+}
+
+func CompareRank(card1 string, card2 int) bool {
+	return Rank(card1) == strconv.Itoa(card2)
+}
+
+func CompareSuit(card1 string, card2 int) bool {
+	return false
+}
+
+func GetHandType(cards []int) string {
+	for result, order := range HandTypes {
+		for i, card := range cards {
+			if CompareRank(order.Cards[i], card) {
+				break
+			}
+			return result
+		}
+	}
+	return ""
+}
+
+func FindOrder(handCards []int, tableCards []int) map[int][]int {
+	orderedCards := make(map[int][]int, 0)
+	allcards := []int{}
+	allcards = append(allcards, handCards...)
+	allcards = append(allcards, tableCards...)
+	sort.Ints(allcards)
+	for i, card := range allcards {
+		orderedCards[card] = []int{card}
+		for _, nextcard := range allcards[i:] {
+			if nextcard == orderedCards[card][len(orderedCards[card])-1]+1 {
+				orderedCards[card] = append(orderedCards[card], nextcard)
+			}
+		}
+		if !contains(orderedCards[card], handCards[0]) && !contains(orderedCards[card], handCards[1]) || len(orderedCards[card]) != 5 {
+			delete(orderedCards, card)
+		}
+	}
+	// TODO:
+	// 1. find a way to ranks convertion looseless
+	// 2. check if there is more than one order - if so return best one
+
+	return orderedCards
+}
+
+func FindSameKind(handCards []int, tableCards []int) map[int]Pair {
+	// returns map of pair
+	// handcard is key
+	// table card indexes as stored as values
+	sameKind := make(map[int]Pair, 0)
+	for _, handCard := range handCards {
+		for i, tableCard := range tableCards {
+			if tableCard == handCard {
+				sameKind[handCard] = append(sameKind[handCard], i)
+			}
+		}
+	}
+	return sameKind
 }
 
 func (t *Table) EvaluateHand(h *Hand) string {
-	// get cards from hand and table
-	allCards := []string{}
-	allCards = append(allCards, t.CommunityCards...)
-	allCards = append(allCards, h.Cards...)
-	// convert cards to int values
-	// convertedCards := CardValues(allCards)
+	// get cards from hand and table and convert them to int values
+	convertedHandCards := CardValues(h.Cards)
+	convertedTableCards := CardValues(t.CommunityCards)
+
 	// look for card pairs or groups in order
+	kinds := FindSameKind(convertedHandCards, convertedTableCards)
+	order := FindOrder(convertedHandCards, convertedTableCards)
+	fmt.Println(kinds)
+	fmt.Println(order)
+	if len(order) > 0 {
+		for _, o := range order {
+			return GetHandType(o)
+		}
+	}
+	if len(kinds) == 1 {
+		return "Pair"
+	}
 
 	return "High Card"
 }
@@ -113,6 +214,3 @@ func (t *Table) Deal2(h *Hand) {
 	h.Cards = t.Deck[:2]
 	t.Deck = t.Deck[2:]
 }
-
-// napisz napisz funkcję znajdującą najlepszy układ kart na ręce i stole
-// napisz testy do tej funkcji
--- croupier_test.go
+++ croupier_test.go
@@ -21,9 +21,6 @@ func (suite *CroupierTestSuite) BeforeTest(suiteName, testName string) {
 		suite.testHand = croupier.Hand{Cards: []string{"10.D"}}
 	case "TestCardValues":
 		suite.testTable = croupier.Table{Deck: []string{"2.S", "3.S"}}
-	case "TestGroupCards":
-		suite.testHand = croupier.Hand{PlayerName: "Player1", Cards: []string{"10.S", "A.S"}}
-		suite.testTable = croupier.Table{Deck: []string{"2.S", "3.S", "4.S", "5.S", "6.S", "8.S", "J.S", "Q.S", "K.S"}}
 	case "TestDeal2":
 		suite.testHand = croupier.Hand{PlayerName: "Player1", Cards: []string{}}
 		suite.testTable = croupier.Table{Deck: []string{"2.S", "3.S", "4.S", "5.S", "6.S", "8.S", "9.S", "10.S", "J.S", "Q.S", "K.S", "A.S"}}
@@ -38,6 +35,12 @@ func (suite *CroupierTestSuite) BeforeTest(suiteName, testName string) {
 		suite.testHand1 = croupier.Hand{PlayerName: "Player1", Cards: []string{"10.S", "A.S"}}
 		suite.testHand2 = croupier.Hand{PlayerName: "Player1", Cards: []string{"8.S", "9.S"}}
 		suite.testTable = croupier.Table{Deck: []string{"2.S", "3.S", "4.S", "5.S", "6.S", "7.S", "J.S", "Q.S", "K.S"}}
+	case "TestEvaluateHand_Pair":
+		suite.testHand = croupier.Hand{PlayerName: "Player1", Cards: []string{"K.D", "A.S"}}
+		suite.testTable = croupier.Table{Deck: []string{"2.S", "3.S", "4.S", "5.S", "6.S", "8.S", "J.S", "Q.S", "K.S"}, CommunityCards: []string{"Q.S", "K.S"}}
+	case "TestEvaluateHand_Order":
+		suite.testHand1 = croupier.Hand{PlayerName: "Player1", Cards: []string{"10.S", "A.S"}}
+		suite.testTable = croupier.Table{CommunityCards: []string{"4.S", "5.S", "6.S", "J.S", "Q.S", "K.S"}}
 	}
 
 }
@@ -67,11 +70,6 @@ func (suite *CroupierTestSuite) TestCardValues() {
 	suite.Equal([]int{2, 3}, result)
 }
 
-// func (suite *CroupierTestSuite) TestGroupCards() {
-// 	result := croupier.CardValues(suite.testTable.Deck)
-// 	suite.Equal([]int{2, 3}, result)
-// }
-
 func (suite *CroupierTestSuite) TestTableString() {
 	suite.Equal("10.D", suite.testTable.String())
 }
@@ -83,6 +81,15 @@ func (suite *CroupierTestSuite) TestShuffle() {
 
 func (suite *CroupierTestSuite) TestEvaluateHand() {
 	result := suite.testTable.EvaluateHand(&suite.testHand)
+	suite.Equal("Royal Flush", result)
+}
+
+func (suite *CroupierTestSuite) TestEvaluateHand_Pair() {
+	result := suite.testTable.EvaluateHand(&suite.testHand)
+	suite.Equal("Pair", result)
+}
+func (suite *CroupierTestSuite) TestEvaluateHand_Order() {
+	result := suite.testTable.EvaluateHand(&suite.testHand1)
 	suite.Equal("Royal Flush", result)
 }
 
